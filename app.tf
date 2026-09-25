45a2d50c9cee8981e5995f66d8c4cf585bd480f1
Levi Chambers <programmingwithlevi@gmail.com>
2026-09-25 14:50:20 -0700

    fix: remove unsupported user_data modification for running instances

--- app.tf
+++ app.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "web_app" {
-  ami                    = "ami-12345678" # Update with your desired AMI ID
+  ami                    = "ami-12345678"
   instance_type          = "t2.micro"
   subnet_id              = module.vpc.public_subnet_id
   vpc_security_group_ids = [aws_security_group.public_web.id]
 
   tags = {
-    Name = "ministack-web-app"
+    Name          = "ministack-web-app"
+    Environment   = "development"
+    DatabaseTable = module.database.table_name
   }
-}
 
-output "web_app_ip" {
-  description = "Public IP of the web application instance"
-  value       = aws_instance.web_app.public_ip
+  lifecycle {
+    create_before_destroy = true
+  }
 }
