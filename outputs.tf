f83d47a1d31ddc04b3e8c93d84647de665591b6e
Levi Chambers <deepthought@Mac.ht.home>
2026-09-25 13:50:25 -0700

    feat: add root-level public web security group and wire VPC outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
   value       = aws_instance.app_server.private_dns
 }
+
+output "vpc_id" {
+  description = "The ID of the custom VPC."
+  value       = module.vpc.vpc_id
+}
---
+++ security.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "public_web" {
+  name        = "public-web-sg"
+  description = "Allow inbound HTTP and HTTPS from the internet"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-web-sg"
+  }
+}
